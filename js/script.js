1b650b9f179aa36eba7da31ccce64c22500de832
Eyzra <eyzra@users.noreply.github.com>
2020-10-27 21:12:14 +0100

    Modified files

--- js/script.js
+++ js/script.js
@@ -4,9 +4,7 @@ var lightIcon = document.querySelector(".fa-sun");
 var darkIcon = document.querySelector(".fa-moon");
 var body = document.querySelector('body')
 var headText = document.querySelector('.about-title h1')
-var nav = document.querySelector('nav ul')
-var logo = document.querySelectorAll('.dev-icon')
-var form = document.querySelector('.contact-section');
+var logo = document.querySelector('.logo')
 
 darkIcon.addEventListener("click", darkMode)
 
@@ -15,17 +13,15 @@ function lightMode(){
     darkIcon.setAttribute("style", "visibility: visible")
     body.setAttribute("style", "background-color: rgba(253, 182, 155, 0.76); color: black;");
     headText.setAttribute("style", "color:black;");
-    form.setAttribute('style', "background-color: white; color:black")
-
-    // for(let i = 0; i < logo.length; i++){
-    //     logo.setAttribute("style", "color: darkblue");
-    // }
+    logo.setAttribute("style", "background-image: linear-gradient(red 0%, coral 25%,brown 50%, deeppink 100%); background-repeat: repeat; -webkit-background-clip: text;  -webkit-text-fill-color: transparent; ")
 }
 
 function darkMode(){
     darkIcon.setAttribute("style", "visibility: hidden")
     lightIcon.setAttribute("style", "visibility: visible")
     body.removeAttribute("style");
+    headText.removeAttribute("style");
+    logo.removeAttribute("style");
 }
 
 
